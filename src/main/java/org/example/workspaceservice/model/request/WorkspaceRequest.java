cb69635403cc881937d508657010709363d0c173
Megamind <soy.tola9999@gmail.com>
2024-11-05 16:57:12 +0700

    modify feedback

--- src/main/java/org/example/workspaceservice/model/request/WorkspaceRequest.java
+++ src/main/java/org/example/workspaceservice/model/request/WorkspaceRequest.java
@@ -13,10 +13,6 @@ public class WorkspaceRequest {
     @NotBlank(message = "Workspace cannot be blank. Please provide a title.")
     @NotNull(message = "Workspace cannot be null. A valid workspace is required.")
     @NotEmpty(message = "Workspace cannot be empty. A valid workspace is required.")
-    @Size(min = 2, max = 255, message = "Workspace length must be between 2 and 255 characters.")
-    @Pattern(
-            regexp = "^(?!\\s)(?!.*\\s{2}).*[A-Za-z0-9 ]*(?<!\\s)$",
-            message = "Workspace cannot start or end with a space, and it cannot contain consecutive spaces."
-    )
+    @Size(max = 255, message = "Workspace length must be between 2 and 255 characters.")
     private String workspaceName;
 }
